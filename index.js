0c366080c36ad751c79def8c734bdfe907ac3efb
Tim <mohamud.hussein@student.moringaschool.com>
2023-06-16 21:33:16 +0300

    first assignment

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let message;
+  if (feet <= 400) {
+    message = "This one is on me!";
+  } else if (feet > 2000) {
+    message = "I will gladly take your thirty bucks.";
+  } else {
+    message = "I am sorry, we do not have a pricing option for that distance.";
+  }
+  return message;
+}
+function scuberGreetingForFeet(feet) {
+  let message;
+  if (feet <= 400) {
+    message = "This one is on me!";
+  } else if (feet > 2000 && feet <= 2500) {
+    message = "I will gladly take your thirty bucks.";
+  } else {
+    message = "No can do.";
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+
+  
+
+
+
+  
